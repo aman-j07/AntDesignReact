e89cf976d3f892be7d2e3848fc6095120e6eae82
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-12-26 18:56:52 +0530

    Ant design

--- src/App.js
+++ src/App.js
@@ -1,52 +1,80 @@
-import 'antd/dist/reset.css';
-import './App.css';
-import { Table } from 'antd';
-import { useEffect, useState } from 'react';
+import "antd/dist/reset.css";
+import "./App.css";
+import { Table, Select, Col, Divider, Row } from "antd";
+import { useEffect, useState } from "react";
 
 const columns = [
   {
-    title: 'ID',
-    dataIndex: 'id',
-    key: 'id',
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
   },
   {
-    title: 'Email',
-    dataIndex: 'email',
-    key: 'email',
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
   },
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
   },
   {
-    title: 'Action',
-    dataIndex: '',
-    key: 'x',
+    title: "Action",
+    dataIndex: "",
+    key: "x",
     render: () => <a>Delete</a>,
   },
 ];
 
 function App() {
-  const [data,setData]=useState([])
-  
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/comments')
-    .then((res) => res.json())
-    .then((data) => {data.forEach((ele)=>{ele.key=ele.id});setData(data);console.log(data)});
-  },[])
-  
+  const [data, setData] = useState([]);
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/comments?_limit=50")
+      .then((res) => res.json())
+      .then((data) => {
+        data.forEach((ele) => {
+          ele.key = ele.id;
+          options.push({
+            value: ele.id,
+            label: ele.name,
+          });
+        });
+        setData(data);
+        setOptions([...options]);
+      });
+  }, []);
+
   return (
     <div className="App">
+      <h2 className="head">Task 1 - Select and Grid</h2>
+      <Divider orientation="left">Select</Divider>
+      <Select mode="multiple" allowClear style={{ width: "100%" }} placeholder="Please select" options={options} gutter="2"/>
+      <Divider orientation="left">Grid</Divider>
+      <Row gutter={[12, 24]} >
+        {data.map((item) => {
+          return (
+            <Col key={item.id} className="gutter-row" xs={24} sm={24} md={12} lg={8} xl={6} xxl={4}><div className="box">{item.email}</div></Col>
+          );
+        })}
+      </Row>
+      <hr/>
+      <h2 className="head">Task 2 - Table with jsonplaceholder API</h2>
+      <Divider orientation="left">Table</Divider>
       <Table
-    columns={columns}
-    expandable={{
-      expandedRowRender: (record) => (<span>{record.id} - {record.email} - {record.name} - {record.body}</span>
-      ),
-      rowExpandable: (record) => record.id !== 'Not Expandable',
-    }}
-    dataSource={data}
-  />
+        columns={columns}
+        expandable={{
+          expandedRowRender: (record) => (
+            <span>
+              {record.id} - {record.email} - {record.name} - {record.body}
+            </span>
+          ),
+          rowExpandable: (record) => record.id !== "Not Expandable",
+        }}
+        dataSource={data}
+      />
     </div>
   );
 }
